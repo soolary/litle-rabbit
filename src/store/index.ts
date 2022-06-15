cb7d16336410495579a3be36f8ed9edec1d4af1f
wangjie <770499571@qq.com>
2022-06-16 00:28:21 +0800

    init

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,8 +2,12 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    userName: '张三'
   },
   getters: {
+    newName (state) {
+      return state.userName + '!!!'
+    }
   },
   mutations: {
   },
